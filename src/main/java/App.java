23202da1ec477c08a895e0bef8c0a0b7dbcdfb04
SeyithanDilek <iletisim.seyithandilek@gmail.com>
2021-06-15 16:22:40 +0300

    Main class added

---
+++ src/main/java/App.java
@@ -0,0 +1,53 @@
+
+
+import com.azure.storage.blob.*;
+import com.azure.storage.blob.models.*;
+import java.io.*;
+
+
+
+public class App {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println( "Hello World!" );
+        String connectStr = "DefaultEndpointsProtocol=https;" +
+                            "AccountName=quick;" +
+                            "AccountKey=yGnAKgqP9LGS5gP13FpdjzBhAWk93VqcPmyEIQlCmCW57VRCMby2b1ulFWogQC2WAVffzZwpx2s/NakQbg0kKA==;" +
+                            "EndpointSuffix=core.windows.net";
+
+        // Bir container client oluşturmak için kullanılacak bir BlobServiceClient nesnesi oluşturulur
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectStr).buildClient();
+
+        //Unique bir container ismi bulunur
+        String containerName = "quickstartblobs2";
+
+        //Container olusturulur ve sonrasinda bir container client nesnesi döndürülür
+        BlobContainerClient containerClient = blobServiceClient.createBlobContainer(containerName);
+
+        // Test icin localde data adlı bir dizin olusturulur(optin)
+        String localPath = "./data/";
+        String fileName = "quickstart" + java.util.UUID.randomUUID() + ".txt";
+        File localFile = new File(localPath + fileName);
+
+        // Dosya icerigi yazdirilir(optin)
+        FileWriter writer = new FileWriter(localPath + fileName, true);
+        writer.write("Hello, World!");
+        writer.close();
+
+        // Blob refernas linki cagrilir(optin)
+        BlobClient blobClient = containerClient.getBlobClient(fileName);
+        System.out.println("\nUploading to Blob storage as blob:\n\t" + blobClient.getBlobUrl());
+
+        // Blob guncellenir
+        blobClient.uploadFromFile(localPath + fileName);
+        System.out.println("\nListing blobs...");
+
+
+        // Blob belirtilen konuma indirilir
+        String downloadFileName = fileName.replace(".txt", "DOWNLOAD.txt");
+        File downloadedFile = new File(localPath + downloadFileName);
+        System.out.println("\nDownloading blob to\n\t " +downloadedFile.getPath());
+        blobClient.downloadToFile(downloadedFile.getPath());
+
+    }
+}
